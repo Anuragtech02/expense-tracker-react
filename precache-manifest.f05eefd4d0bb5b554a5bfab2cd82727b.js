623163360904e7b7ab060484b0534e48b19e9b45
Anurag Pal <apal895@gmail.com>
2020-06-03 18:25:43 +0530

    Updates

---
+++ precache-manifest.f05eefd4d0bb5b554a5bfab2cd82727b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "514488f02ed41d37164a3346892f76cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d27780fc3575e3c8d812",
+    "url": "/static/css/main.23de9268.chunk.css"
+  },
+  {
+    "revision": "654e4d2694fd77a4b314",
+    "url": "/static/js/2.10037211.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.10037211.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d27780fc3575e3c8d812",
+    "url": "/static/js/main.0ac81bc3.chunk.js"
+  },
+  {
+    "revision": "0fb5aa10d11e0a3e8d13",
+    "url": "/static/js/runtime-main.56c06abf.js"
+  }
+]);
